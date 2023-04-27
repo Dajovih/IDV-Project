c1c75ef3e048c657ac196140eb9ea448c28e7638
Dajovih <jcardonaso@unal.edu.co>
2023-04-26 22:59:19 -0500

    Player movement
    Script with the code for the player movement

---
+++ Assets/Scripts/PlayerMovement.cs
@@ -0,0 +1,11 @@
+using UnityEngine;
+public class PlayerMovement : MonoBehaviour
+{
+    [SerializeField]
+    private float _speed = 1;
+    void Update()
+    {
+        float horizontalIn = Input.GetAxisRaw("Horizontal");
+        transform.position += new Vector3(horizontalIn*_speed*Time.deltaTime,0f,0f); 
+    }
+}
