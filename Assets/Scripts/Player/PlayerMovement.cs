5ce26d7b6b30cde32461a37bc7240121e8ff651e
José David <jcardonaso@unal.edu.co>
2023-06-19 13:33:31 -0500

    Player Animations
    Creation of each one of the animations: Run, jump, idle, fall, another ones. Logic of the animations in progress

--- Assets/Scripts/Player/PlayerMovement.cs
+++ Assets/Scripts/Player/PlayerMovement.cs
@@ -6,27 +6,31 @@ public class PlayerMovement : MonoBehaviour {
     //Par�metros de salto y velocidad. Ajustarlo en interfaz
     [SerializeField] private float _speed;
     [SerializeField] private float _jumpForce;
+    [SerializeField] private Animator _animator;
+
 
     private Rigidbody2D _body;
     private bool _inGround; //Para verificar que solo puede saltar si se encuentra en el suelo
 
-    private void Awake() {
+    private void Start() {
         _body = GetComponent<Rigidbody2D>();
+        _animator = GetComponent<Animator>();
     }
 
     private void Update() {
         float direction = Input.GetAxis("Horizontal"); //Obtener la direccion hacia la cual se est� movimendo : (1,-1)
+        _animator.SetFloat("Movement", MathF.Abs(direction)); 
         _body.velocity = new Vector2(_speed * direction, _body.velocity.y); //Aplicar desplazamiento en la coordenada x teniendo en cuenta la velocidad y direcci�n
-
         if (direction > 0f) { //Voltear al jugador seg�n la direcci�n hacia la cual se est� moviendo
             transform.localScale = Vector3.one;
         } else if (direction < 0f) {
             transform.localScale = new Vector3(-1, 1, 1);
         }
-
+        
         if (Input.GetKey(KeyCode.Space) && _inGround) {  //Salto
             Jump();
         }
+        
     }
 
     private void Jump() {
